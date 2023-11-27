06d8090364ada5e6f51cd1dce0ae59bde934c505
Josh Nussbaum <joshnuss@gmail.com>
2023-11-26 19:22:53 -0500

    Adds more gerber support

--- src/formats/GerberWriter.js
+++ src/formats/GerberWriter.js
@@ -18,6 +18,11 @@ const plotStates = {
   circular_counter_clockwise: '03'
 }
 
+const apertureTypes = {
+  circle: 'C',
+  rect: 'R'
+}
+
 export default class GerberWriter {
   constructor(board, options) {
     this.board = board
@@ -26,7 +31,7 @@ export default class GerberWriter {
   }
 
   async write(folder) {
-    const { layers } = this.board
+    const { outline, layers } = this.board
 
     await this.#writeFile(folder, 'F_Silkscreen.gbr', 'Legend,Bot', layers.silkscreen.top)
     await this.#writeFile(folder, 'B_Silkscreen.gbr', 'Legend,Top', layers.silkscreen.bottom)
@@ -36,6 +41,11 @@ export default class GerberWriter {
     await this.#writeFile(folder, 'B_Paste.gbr', 'Paste,Bot', layers.paste.bottom)
     await this.#writeFile(folder, 'F_Cu.gbr', 'Copper,L1,Top', layers.copper.top)
     await this.#writeFile(folder, 'B_Cu.gbr', 'Copper,L2,Bot', layers.copper.bottom)
+    await this.#writeFile(folder, 'Edge_Cuts.gbr', 'Profile', outline)
+  }
+
+  #coordinate({ x, y }) {
+    return { x, y: this.board.height - y }
   }
 
   async #writeFile(folder, file, functions, layer) {
@@ -60,19 +70,68 @@ export default class GerberWriter {
       g.polarity('dark')
       g.plotState('linear')
 
+      g.comment('Aperture list')
+      g.comment('Aperture macro list')
+
+      g.comment('Aperture macro list end')
+
+      layer.forEach((shape, index) => {
+        if (shape.type === 'rect') {
+          const size = `${shape.width}X${shape.height}`
+
+          g.apertureDefinition(index + 10, shape.type, size)
+        } else if (shape.type === 'circle') {
+          g.apertureDefinition(index + 10, shape.type, shape.r * 2)
+        } else if (shape.type === 'polyline') {
+          g.apertureDefinition(index + 10, 'circle', shape.stroke)
+        }
+      })
+
+      g.comment('Aperture list end')
+      g.blankLine()
+
+      layer.forEach((shape, index) => {
+        const designator = `X${index + 1}`
+        g.comment(`Starting ${shape.type} ${designator}`)
+        g.selectAperture(index + 10)
+        g.objectAttribute('C', designator)
+
+        if (shape.type === 'rect') {
+          g.flash(this.#coordinate(shape))
+        } else if (shape.type === 'circle' ) {
+          g.flash(this.#coordinate({ x: shape.cx, y: shape.cy }))
+        } else if (shape.type === 'polyline') {
+          const [first, ...rest] = shape.points
+
+          g.moveTo(this.#coordinate(first))
+
+          rest.forEach((point) => g.lineTo(this.#coordinate(point)))
+        }
+
+        g.deleteAllAttributes()
+        g.comment(`Ending ${shape.type} ${designator}`)
+        g.blankLine()
+      })
+
       g.endOfFile()
     })
   }
 }
 
 function gerberStream(stream) {
+  let macro = false
+
   return {
     attribute(key, ...values) {
       stream.write(`%TF.${key},${values.join(',')}*%\n`)
     },
 
     comment(text) {
-      stream.write(`G04 ${text}*\n`)
+      if (macro) {
+        stream.write(`0 ${text}*\n`)
+      } else {
+        stream.write(`G04 ${text}*\n`)
+      }
     },
 
     specification(option, format) {
@@ -91,8 +150,60 @@ function gerberStream(stream) {
       stream.write(`G${plotStates[plotState]}*\n`)
     },
 
+    startMacro(name) {
+      macro = true
+
+      stream.write(`%AM${name}*\n`)
+    },
+
+    apertureAttribute(key, ...values) {
+      stream.write(`%TA.${key},${values.join(',')}*%\n`)
+    },
+
+    apertureDefinition(number, type, size) {
+      stream.write(`%ADD${number}${apertureTypes[type]},${size}*%\n`)
+    },
+
+    deleteAllAttributes() {
+      stream.write('%TD*%\n')
+    },
+
+    selectAperture(number) {
+      stream.write(`D${number}*\n`)
+    },
+
+    objectAttribute(type, value) {
+      stream.write(`%TO.${type},${value}*%\n`)
+    },
+
+    flash({ x, y }) {
+      stream.write(`X${formatNumber(x)}Y${formatNumber(y)}D03*\n`)
+    },
+
+    moveTo({ x, y }) {
+      stream.write(`X${formatNumber(x)}Y${formatNumber(y)}D02*\n`)
+    },
+
+    lineTo({ x, y }) {
+      stream.write(`X${formatNumber(x)}Y${formatNumber(y)}D01*\n`)
+    },
+
+    blankLine() {
+      stream.write('\n')
+    },
+
     endOfFile() {
-      stream.write(`M02*\n`)
+      stream.write('M02*\n')
     }
   }
+}
+
+function formatNumber(number, format = numberFormat) {
+  return number.toLocaleString(undefined, {
+    useGrouping: false,
+    minimumSignificantDigitsDigits: format.integer,
+    maximumSignificantDigitsDigits: format.integer,
+    minimumFractionDigits: format.decimal,
+    maximumFractionDigits: format.decimal
+  }).replace('.', '')
 }
